68256bbf243cd1215e6d9a642759a0ec07d96847
Lucas Vieira <ls6182315@gmail.com>
2022-12-30 00:22:08 +0000

    add const ok status

--- src/routes/talkersRoutes.js
+++ src/routes/talkersRoutes.js
@@ -2,6 +2,8 @@ const { Router } = require('express');
 
 const router = Router();
 
+const HTTP_OK_STATUS = 200;
+
 router.get('/talker', async (_req, res) => {
     const talker = await readTalkers();
     return res.status(HTTP_OK_STATUS).json(talker);
