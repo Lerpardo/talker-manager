c92b9bd14430b8166105322daa3d782bb0a2f557
Lucas Vieira <ls6182315@gmail.com>
2022-12-30 00:27:52 +0000

    GET talker by id

--- src/routes/talkersRoutes.js
+++ src/routes/talkersRoutes.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const {readTalkers,talkerById} = require('../utils/fsUtils')
 
 const router = Router();
 
@@ -7,4 +8,16 @@ const HTTP_OK_STATUS = 200;
 router.get('/talker', async (_req, res) => {
     const talker = await readTalkers();
     return res.status(HTTP_OK_STATUS).json(talker);
+  });
+
+  router.get('/talker/:id', async (req, res) => {
+    const { id } = req.params;
+    const talkersData = await talkerById(id);
+    
+    if (talkersData === undefined) {
+      return res.status(404).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  }
+    res.status(HTTP_OK_STATUS).json(talkersData);
   });
