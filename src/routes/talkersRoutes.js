fb9511d42b068af887699f1d06d3c6afd0c536f8
Lucas Vieira <ls6182315@gmail.com>
2022-12-30 00:58:23 +0000

    fixing routes

--- src/routes/talkersRoutes.js
+++ src/routes/talkersRoutes.js
@@ -17,18 +17,18 @@ const router = Router();
 
 const HTTP_OK_STATUS = 200;
 
-router.get('/talker', async (_req, res) => {
+router.get('/', async (_req, res) => {
     const talker = await readTalkers();
     return res.status(HTTP_OK_STATUS).json(talker);
 });
 
-router.get('/talker/search', validateToken, async (req, res) => {
+router.get('/search', validateToken, async (req, res) => {
     const { q } = req.query;
     const TalkerQ = await queryTalkers(q);
     return res.status(HTTP_OK_STATUS).json(TalkerQ);
   });
 
-router.get('/talker/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
     const { id } = req.params;
     const talkersData = await talkerById(id);
 
@@ -40,7 +40,7 @@ router.get('/talker/:id', async (req, res) => {
     res.status(HTTP_OK_STATUS).json(talkersData);
 });
 
-router.post('/talker', validateToken,
+router.post('/', validateToken,
     validateName, validateAge, validateTalk, validateWatchedAt, validateRate, async (req, res) => {
         const talkerData = req.body;
         const talkers = await addNewTalkers(talkerData);
@@ -48,7 +48,7 @@ router.post('/talker', validateToken,
     });
 
 
-router.put('/talker/:id', validateToken,
+router.put('/:id', validateToken,
     validateName, validateAge, validateTalk, validateWatchedAt, validateRate, async (req, res) => {
         const { id } = req.params;
         const newUpTalker = req.body;
@@ -56,7 +56,7 @@ router.put('/talker/:id', validateToken,
         return res.status(200).json(newUpData);
     });
 
-router.delete('/talker/:id', validateToken, async (req, res) => {
+router.delete('/:id', validateToken, async (req, res) => {
     const { id } = req.params;
     await deleteTalkerById(id);
     res.status(204).end();
