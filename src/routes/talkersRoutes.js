a1701ea60f9999701d4b9b1127e7f25eb94fa4ec
Lucas Vieira <ls6182315@gmail.com>
2022-12-30 00:35:39 +0000

    post login and token generator

--- src/routes/talkersRoutes.js
+++ src/routes/talkersRoutes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
-const {readTalkers,talkerById} = require('../utils/fsUtils')
+const { readTalkers, talkerById,tokenGenerator } = require('../utils/fsUtils')
+const {validateLoginData } = require('../middlewares/validateLogin')
 
 const router = Router();
 
@@ -8,16 +9,21 @@ const HTTP_OK_STATUS = 200;
 router.get('/talker', async (_req, res) => {
     const talker = await readTalkers();
     return res.status(HTTP_OK_STATUS).json(talker);
-  });
+});
 
-  router.get('/talker/:id', async (req, res) => {
+router.get('/talker/:id', async (req, res) => {
     const { id } = req.params;
     const talkersData = await talkerById(id);
-    
+
     if (talkersData === undefined) {
-      return res.status(404).json({
-      message: 'Pessoa palestrante não encontrada',
-    });
-  }
+        return res.status(404).json({
+            message: 'Pessoa palestrante não encontrada',
+        });
+    }
     res.status(HTTP_OK_STATUS).json(talkersData);
-  });
+});
+
+router.post('/login', validateLoginData, async (_req, res) => {
+    const tokenGenerated = { token: tokenGenerator() };
+    return res.status(HTTP_OK_STATUS).json(tokenGenerated);
+});
